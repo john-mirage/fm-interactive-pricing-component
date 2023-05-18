944e726b10a09a7188f57948a1f8248fe769c2ee
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-18 21:52:52 +0200

    fix: remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fm-interactive-pricing-component/",
   plugins: [react({
     babel: {
       plugins: [
